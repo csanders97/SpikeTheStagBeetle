874b455dbc93d072adee178628596f56c4135f40
Justine Barry <justinianfruit@gmail.com>
2017-07-19 14:34:39 -0600

    Updated clearCanvas function and button. Also updated the DrawView import in main/res/layout/activity_main.xml.

--- GroupPaint/app/src/main/java/com/example/csanders/grouppaintproject/MainActivity.java
+++ GroupPaint/app/src/main/java/com/example/csanders/grouppaintproject/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.csanders.grouppaintproject;
 
+import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CanvasView customCanvas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        customCanvas = (CanvasView) findViewById(R.id.signature_canvas);
+    }
+
+    public void clearCanvas(View view) {
+        customCanvas.clearCanvas();
     }
 }
